d4f99f10e1805feb498ce1497803514523c16bcd
Pedro <pedrodelesporte@outlook.com>
2025-12-24 12:59:13 -0500

    *

---
+++ assignments/abstract-classes/Triangle.java
@@ -0,0 +1,55 @@
+public class Triangle extends GeometricObject {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle() {
+        this.side1 = 1.0;
+        this.side2 = 1.0;
+        this.side3 = 1.0;
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public Triangle(double side1, double side2, double side3, String color, boolean filled) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    // heron's formula
+    @Override
+    public double getArea() {
+        double s = getPerimeter() / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("side1=%6.2f, side2=%6.2f, side3=%6.2f, color=%s, filled=%b",
+                side1, side2, side3, getColor(), isFilled());
+    }
+}
